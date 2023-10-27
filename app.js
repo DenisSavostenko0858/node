744691cc03c768e85b8619176daf0f4f258ee431
DenisSavostenko0858 <denyotube0858@gmail.com>
2023-10-27 14:42:58 +0300

    commit

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-import express from "express";
+const express = require("express")
+const favicon = require("express-favicon");
+
+const path = require("path");
+
 const app = express();
+
+app.use(express.static(path.join(__dirname,'public')))
+
+console.log(__dirname + "/public/favicon.ico")
+
+
+app.use(favicon(__dirname + "/public/favicon.ico"))
 const port = '3000';
 
 app.get('/', function (req, res){
-    res.end('Hello world');
+    res.sendFile(__dirname + "/public/index.html");
     // console.log(req) //Вывод к нам в консоль не в браузер
 })
 
