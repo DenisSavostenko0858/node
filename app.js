29e891fac2ffe31e7cc80a8a83e58ac4d9eda846
DenisSavostenko0858 <denyotube0858@gmail.com>
2023-12-15 13:30:46 +0300

    commit

--- app.js
+++ app.js
@@ -1,79 +1,96 @@
-const { error } = require("console");
-const express = require("express")
+const express = require("express");
 const favicon = require("express-favicon");
-const fs = require("fs")
-const { appendFile } = require("fs");
+const fs = require("fs");
 const path = require("path");
-const app = express();
+const { nextTick } = require("process");
 const ejs = require("ejs");
 const session = require("express-session");
-const userSession = require("./middleware/user_session");
-const port = '3000';
-const myrouts = require("./routers/index_routers");
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
+const myRoutes = require("./routers/index_routers");
+const userSession = require("./middleware/user_session");
+const port = "3000";
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const filePath = path.join(__dirname, "tmp", "1.txt");
+
+// fs.writeFile(filePath, `Сервер запущен. Порт: ${port}`, (err) => {
+//   if (err) console.error(err);
+//   console.log("файл создан");
+// });
+
+// function logger(port, router) {
+//   fs.appendFile(
+//     filePath,
+//     `\nЛогируем ping по адресу localhost:${port}${router}. Время: ${new Date()}`,
+//     (err) => {
+//       if (err) console.error(err);
+//       console.log("файл переписан");
+//     }
+//   );
+// }
+
+// console.log(app.get("env"));
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.use(
-    session({
-        secret: "aboba",
-        resave: false,
-        saveUninitialized: true,
-    })
-    );
-    app.use(userSession);
-    app.use(myrouts);
-    app.use(express.urlencoded((extended = true)));
-//Доступ к папке на прямую
-app.use(express.static(path.join(__dirname,'public')));
-app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.static(path.join(__dirname, 'public/css/bootstrap-5.3.2/dist/css')));
-//scss
-app.use(express.static(path.join(__dirname, 'public/css/bootstrap-5.3.2/dist/css/bootstrap.css')))
+  session({
+    secret: "aboba",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-//Иконка
-console.log(__dirname + "/public/favicon.ico")
-app.use(favicon(__dirname + "/public/favicon.ico"))
+app.use(
+  "/css/bootstrap.css",
+  express.static(
+    path.join(
+      __dirname,
+      "public/css/bootstrap-5.3.2/dist/css/bootstrap.min.css"
+    )
+  )
+);
 
+app.use(favicon(__dirname + "/public/favicon.png"));
+app.use(userSession);
+app.use(myRoutes);
 
-//Создаем файл
-// function addLine(line){
-//     line = line + " timestamp: " + new Date().toLocaleString();
-//     fs.appendFile(
-//         path.join(__dirname + "/public/logger.txt"),
-//         line + "\n",
-//         (err) => {
-//            if(err) console.log(err) 
-//         }
-//     )
-// };
-
-//error hundler
+app.listen(port, () => {
+  console.log(`listen on port ${port}`);
+});
+app.get("env") == "production";
+console.log(app.get("env"));
+if (app.get("env") == "production") {
+  app.use((req, res, err) => {
+    res.status(err.status);
+    res.sendFile(err.message);
+  });
+}
+//ERROR HANDLER
 app.use((req, res, next) => {
-    const err = new Error("NO FOUND ERROR");
-    err.status = 404;
-    next(err);
+  const err = new Error("Could't get path");
+  err.status = 404;
+  next(err);
 });
 
+if (app.get("env") != "development") {
+  app.use(function (err, req, res, next) {
+    console.log(err.status, err.message);
+    res.status = 404;
+    link = "https://centralsib.com/media/gallery/kukushka.jpg";
+    res.render("error.ejs", { err, link });
+  });
+} else {
+  app.use(function (err, req, res, next) {
+    console.log(app.get("env"), err.status, err.message);
+  });
+}
 
-//Если мы зашли не под dev то ошибка будет в виде файла err.js, если dev то консоль лог
-if (app.get("env") != "development"){
-        app.use(function(err, req, res, next){
-            res.status = 404;
-            let img = "https://blog.vverh.digital/wp-content/uploads/2020/06/oblojka-404.png";
-            res.render("err.ejs", {err,img}); //Передаем обьект err
-        })}
-    else{
-        app.use(function(err, req, res, next){
-        console.log(app.get("env"), err.status, err.message);
-        });
-    };        
-
-//Прослушиваем порт
-app.listen(port, function(){
-    console.log("Сервер запущен порт " + port);
-    // addLine("Server started");
-});
+// kjsdhkdsh
--- routers/index_routers.js
+++ routers/index_routers.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const app = express()
+const express = require("express");
 const router = express.Router();
-const register = require('../controlers/register')
-const login = require('../controlers/login')
-const entries = require('../controlers/entries')
+const register = require("../controllers/register");
+const login = require("../controllers/login");
+const logout = require("../controllers/logout");
+const entries = require("../controllers/entries");
+const entry = require("../controllers/entry");
+const post = require("../controllers/post");
+const index = require("../controllers/index");
 
-router.get('/',entries.list);
-router.get('/post', entries.form);
-router.post('/post', entries.submit);
-// Страница после входа         
+router.get("/", entries.list);
 
-router.get('/login',login.form);
-router.post('/login', login.submit);
-// Главная страница добавить кнопки входа и регистрации и блок для вывода данных пользователя
-router.get('/register',register.form);
-router.post('/register', register.submit);
+router.get("/post", entries.form);
+router.post("/post", entries.submit);
 
-router.post('/logout', login.logout);
+router.get("/register", register.form);
+router.post("/register", register.submit);
+
+router.get("/login", login.form);
+router.post("/login", login.submit);
+
+// router.get("/post", post.form);
+// router.post("/post", post.submit);
+
+router.get("/logout", login.logout);
 
 module.exports = router;
