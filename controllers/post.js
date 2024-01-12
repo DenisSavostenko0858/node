bb7d57689a7fbe2e1b4e0083082db2edb015e76c
DenisSavostenko0858 <denyotube0858@gmail.com>
2024-01-12 19:56:28 +0300

    test new branch

---
+++ controllers/post.js
@@ -0,0 +1,8 @@
+// let Massiv = [1,4,6,7];
+// for (var i = 0; i < Massiv.length; i++) {
+//     if (Massiv[i] % 2 == 0) {
+//         console.log(Massiv[i]);
+//     }else if(!Massiv[i] % 2 == 0) {
+//         console.log("Число не четное: " + Massiv[i]);
+//     }
+// }
