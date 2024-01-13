3b25576e043bc5aaa51b35859eab666ae0a30836
DenisSavostenko0858 <denyotube0858@gmail.com>
2024-01-13 14:12:58 +0300

    delete hash password

--- controllers/post.js
+++
@@ -1,8 +0,0 @@
-// let Massiv = [1,4,6,7];
-// for (var i = 0; i < Massiv.length; i++) {
-//     if (Massiv[i] % 2 == 0) {
-//         console.log(Massiv[i]);
-//     }else if(!Massiv[i] % 2 == 0) {
-//         console.log("Число не четное: " + Massiv[i]);
-//     }
-// }
--- models/user.js
+++ models/user.js
@@ -12,12 +12,12 @@ class User {
   constructor() {}
   static async create(dataForm, next, cb) {
     try {
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(dataForm.password, salt);
+      // const salt = await bcrypt.genSalt(10);
+      // const hash = await bcrypt.hash(dataForm.password, salt);
 
       const sql1 =
         "INSERT INTO users (name, email, password, age) VALUES (?, ?, ?, ?)";
-      db.run(sql1, dataForm.name, dataForm.email, hash, dataForm.age, cb);
+      db.run(sql1, dataForm.name, dataForm.email, dataForm.password, dataForm.age, cb);
     } catch (error) {
       if (error) return next(error);
     }
@@ -31,15 +31,11 @@ class User {
     User.findByEmail(dataForm.email, (error, user) => {
       if (error) return cb(error);
       if (!user) return cb();
-
-      const result = bcrypt.compare(
-        dataForm.password,
-        user.password,
-        (err, result) => {
-          if (result) return cb(null, user);
-          cb();
-        }
-      );
+      if (dataForm.password === user.password) { 
+        cb(null, user); 
+      } else { 
+        cb(); 
+      }
     });
   }
 }
